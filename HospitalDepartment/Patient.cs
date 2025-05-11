27f6278ad648c9632c76cf83954eb2aa07aef97d
Lavious <krisily1230943@gmail.com>
2025-05-11 23:35:17 +0300

    Add abstract class and its derivatives

---
+++ HospitalDepartment/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalDepartment
+{
+    public class Patient : Person
+    {
+        public override string Surname { get; set; }
+        public override string Name { get; set; }
+        public override string Patronymic { get; set; }
+        public override string Gender { get; set; }
+        public override int Age { get; set; }
+        public string Diagnosis { get; set; }
+        public string Status { get; set; }
+        public int doctor_person { get; set; }
+
+        public Patient(string surname, string name, string patronymic, string gender, int age, string diagnosis = "", string status = "", int id_doctor = 0)
+        {
+            Surname = surname;
+            Name = name;
+            Patronymic = patronymic;
+            Gender = gender;
+            Age = age;
+            Diagnosis = diagnosis;
+            Status = status;
+            doctor_person = id_doctor;
+        }
+    }
+}
---
+++ HospitalDepartment/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalDepartment
+{
+    public abstract class Person
+    {
+        public abstract string Surname { get; set; }
+        public abstract string Name { get; set; }
+        public abstract string Patronymic { get; set; }
+        public abstract string Gender { get; set; }
+        public abstract int Age { get; set; }
+    }
+}
